7daf74852677b5e68a7b2b4bfbfd7c16f576666a
aseem-karan <ak1679@srmist.edu.in>
2026-03-06 00:49:29 +0530

    UC7: Store character patterns in a class

---
+++ CharacterPatterns.java
@@ -0,0 +1,23 @@
+public class CharacterPatterns {
+
+    public static String[] getA() {
+        return new String[]{
+            "  *  ",
+            " * * ",
+            "*****",
+            "*   *",
+            "*   *"
+        };
+    }
+
+    public static String[] getS() {
+        return new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        };
+    }
+
+}
